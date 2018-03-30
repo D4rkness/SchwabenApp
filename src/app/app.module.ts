81f71ee0e573e9956caabdb3fb66ce00fe015877
Home <misterschinken@hotmail.de>
2018-03-30 16:41:11 +0200

    Added FontAwesome to the App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import {NavbarComponent} from './navbar/navbar.component';
   ],
   imports: [
     NgbModule.forRoot(),
+    AngularFontAwesomeModule,
     BrowserModule
   ],
   providers: [],
